f4fa28bf4fdc4a5a284816e60d99c7f374625cbd
Artem Rogov <artemrogov281@gmail.com>
2026-01-05 22:39:36 +0300

    delete input processor listener

--- core/src/main/java/io/game_3d/artemrogov/Main.java
+++ core/src/main/java/io/game_3d/artemrogov/Main.java
@@ -3,8 +3,6 @@ package io.game_3d.artemrogov;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Files;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.g3d.Environment;
@@ -16,17 +14,14 @@ import com.badlogic.gdx.graphics.g3d.loader.G3dModelLoader;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.UBJsonReader;
 
-
-/**
- * {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms.
- */
-public class Main extends ApplicationAdapter implements InputProcessor {
+public class Main extends ApplicationAdapter {
 
     private PerspectiveCamera perspectiveCamera;
     private ModelBatch modelBatch;
     private Model model;
     private ModelInstance modelInstance;
     private Environment environment;
+
 
     @Override
     public void create() {
@@ -46,14 +41,13 @@ public class Main extends ApplicationAdapter implements InputProcessor {
 
         environment = new Environment();
         environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.8f, 0.8f, 0.8f, 1f)); // сероватый, + интенсивность - 1
-
-        Gdx.input.setInputProcessor(this);
     }
 
     @Override
     public void render() {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+        perspectiveCamera.rotateAround(Vector3.Zero, Vector3.Y, 0.8f);
         perspectiveCamera.update();
         modelBatch.begin(perspectiveCamera);
         modelBatch.render(modelInstance, environment);
@@ -64,56 +58,5 @@ public class Main extends ApplicationAdapter implements InputProcessor {
     public void dispose() { // очистка мусора
         model.dispose();
         modelBatch.dispose();
-    }
-
-    @Override
-    public boolean keyDown(int keycode) {
-        if (keycode == Input.Keys.LEFT) {
-            perspectiveCamera.rotateAround(new Vector3(0f, 0f, 0f), new Vector3(0f, 1f, 0f), 1f);
-        }
-        if (keycode == Input.Keys.RIGHT) {
-            perspectiveCamera.rotateAround(new Vector3(0f, 0f, 0f), new Vector3(0f, -1f, 0f), 1f);
-        }
-        return true;
-    }
-
-    @Override
-    public boolean keyUp(int keycode) {
-        return false;
-    }
-
-    @Override
-    public boolean keyTyped(char character) {
-        return false;
-    }
-
-    @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        return false;
-    }
-
-    @Override
-    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        return false;
-    }
-
-    @Override
-    public boolean touchCancelled(int screenX, int screenY, int pointer, int button) {
-        return false;
-    }
-
-    @Override
-    public boolean touchDragged(int screenX, int screenY, int pointer) {
-        return false;
-    }
-
-    @Override
-    public boolean mouseMoved(int screenX, int screenY) {
-        return false;
-    }
-
-    @Override
-    public boolean scrolled(float amountX, float amountY) {
-        return false;
     }
 }
